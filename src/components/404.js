d4901e91e85c006b3bd70f5c70e81f67d53ed9b2
Akshay Kulkarni <Akshay.Kulkarni@kpit.com>
2018-07-17 13:12:58 +0530

    404 icon, delete react logo

--- src/components/404.js
+++ src/components/404.js
@@ -3,6 +3,7 @@ import React from "react";
 const FourOFour = () => {
   return (
     <div className="not-found">
+      <img src="images/404.svg" alt="NOT FOUND" />
       <h1>404</h1>
       <h4>NOT FOUND</h4>
     </div>
