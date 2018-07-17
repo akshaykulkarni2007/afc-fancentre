6b79703af327896cc6421983fe25d62d952d0a52
Akshay Kulkarni <Akshay.Kulkarni@kpit.com>
2018-07-17 11:57:35 +0530

    added 404

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,36 @@
-import React, { Component } from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // CSS
-import "./App.css"
+import "./App.css";
 
 // Components
-import Navigation from "./Common/Navigation"
-import Footer from "./Common/Footer"
-import Home from "./Homepage/Home"
-import Login from "./Auth/Login"
-import PlayerList from "./Players/PlayerList"
-import About from "./About/About"
+import Navigation from "./Common/Navigation";
+import Footer from "./Common/Footer";
+import Home from "./Homepage/Home";
+import Login from "./Auth/Login";
+import PlayerList from "./Players/PlayerList";
+import About from "./About/About";
+import FourOFour from "./404";
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div className="app">
-					<Navigation />
-					<Route exact path="/" component={Home} />
-					<Route exact path="/players" component={PlayerList} />
-					<Route exact path="/about" component={About} />
-					<Route exact path="/login" component={Login} />
-					<Footer />
-				</div>
-			</Router>
-		)
-	}
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/players" component={PlayerList} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/login" component={Login} />
+            <Route component={FourOFour} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App
+export default App;
