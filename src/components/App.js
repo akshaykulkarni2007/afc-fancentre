4a1ed59e8ad5b0a368f78e9fcb0d0067f3161a68
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2018-07-27 18:02:58 +0530

    sticky footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 // CSS
-import "./App.css";
+import "./App.css"
 
 // Components
 import { Navigation, Footer } from "./Common"
@@ -15,34 +15,36 @@ import About from "./About/About"
 import FourOFour from "./404"
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="app">
-          <Navigation />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/players" component={PlayerList} />
+	render() {
+		return (
+			<Router>
+				<div className="app">
+					<Navigation />
+					<main>
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/players" component={PlayerList} />
 
-			<Route exact path="/club/gallery" component={Gallery}/>
-			<Route exact path="/club/history" component={History}/>
-			<Route exact path="/club/records" component={Records}/>
-			<Route exact path="/club/season" component={Season}/>
+							<Route exact path="/club/gallery" component={Gallery} />
+							<Route exact path="/club/history" component={History} />
+							<Route exact path="/club/records" component={Records} />
+							<Route exact path="/club/season" component={Season} />
 
-			<Route exact path="/fans/feed" component={Feed} />
-			<Route exact path="/fans/media" component={Media} />
-			<Route exact path="/fans/messages" component={Messages} />
-			<Route exact path="/fans/polls" component={Polls} />
+							<Route exact path="/fans/feed" component={Feed} />
+							<Route exact path="/fans/media" component={Media} />
+							<Route exact path="/fans/messages" component={Messages} />
+							<Route exact path="/fans/polls" component={Polls} />
 
-            <Route exact path="/about" component={About} />
-            <Route exact path="/login" component={Login} />
-            <Route component={FourOFour} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+							<Route exact path="/about" component={About} />
+							<Route exact path="/login" component={Login} />
+							<Route component={FourOFour} />
+						</Switch>
+					</main>
+					<Footer />
+				</div>
+			</Router>
+		)
+	}
 }
 
 export default App
